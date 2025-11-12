abf908fabbe9654380c82423a98b3432ad8f70e5
AdeyAmare <amareadey@gmail.com>
2025-11-12 19:43:14 +0300

    fix: plot

--- app/utils.py
+++ app/utils.py
@@ -27,7 +27,7 @@ def plot_boxplot(df_list, selected_names, metric):
         st.warning("No data available for the selected datasets/metric")
         return
 
-    fig, ax = plt.subplots(figsize=(3, 3), dpi=150)  # smaller but higher-res
+    fig, ax = plt.subplots(figsize=(6,4))
 
     sns.boxplot(x='Dataset', y=metric, data=combined_df, ax=ax)
     ax.set_title(f"{metric} Distribution")
